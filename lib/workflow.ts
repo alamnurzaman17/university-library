6ec80bc5e72197b17eecceae47f6c44e9a879895
Alam Nurzaman <alam.nurzaman17@gmail.com>
2025-05-19 02:51:57 +0700

    Add last update function

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,7 @@
-import {Client as WorkflowClient} from "@upstash/workflow"
-import config from "@/lib/config"
+// import {Client as WorkflowClient} from "@upstash/workflow"
+// import config from "@/lib/config"
 
-export const workflowClient  = new WorkflowClient({
-    baseUrl: config.env.upstash.qstashUrl,
-    token: config.env.upstash.qstashToken
-})
+// export const workflowClient  = new WorkflowClient({
+//     baseUrl: config.env.upstash.qstashUrl,
+//     token: config.env.upstash.qstashToken
+// })
